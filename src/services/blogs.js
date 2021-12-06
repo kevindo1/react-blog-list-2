925028196024b9c9ac34c3ee36637be0f4cc70ae
Kevin Do <kevind0501@gmail.com>
2021-12-06 14:56:40 -0800

    removed console logs

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -3,6 +3,5 @@ import { checkError, client } from './client';
 export async function getBlogs() {
   // call supabase and get list of restaurants
   const response = await client.from('blogs').select();
-  console.log(response);
   return checkError(response);
 }
